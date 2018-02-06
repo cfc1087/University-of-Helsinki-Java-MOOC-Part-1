41aa2f8b25c856f009aadd122e3f314c15cb1247
cfc1087 <cfc1087@gmail.com>
2018-02-06 10:07:29 -0500

    Add files via upload

---
+++ week1-003.Spruce/src/Spruce.java
@@ -0,0 +1,19 @@
+
+public class Spruce {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 0; j < 4 - i; j++) {
+                System.out.print(" ");
+
+            }
+            for (int k = 0; k <= 2 * i; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+        System.out.println("    *");
+    }
+}
